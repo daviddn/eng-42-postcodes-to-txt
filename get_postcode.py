0a9d2768573152c7ad2e2d52f2531a2b549b2833
David Naim <DNaim@spartaglobal.com>
2019-10-02 18:10:08 +0100

    added error handling for wrong url

--- get_postcode.py
+++ get_postcode.py
@@ -1,10 +1,13 @@
 import requests
 
 def get_post_code(postcode):
-    request_postcode = requests.get('http://api.postcodes.io/postcodes/' + postcode)
-    converted_postcode = request_postcode.json()
-    postcode = (converted_postcode['result']['postcode'])
-    return postcode
+    # try:
+        request_postcode = requests.get('http://api.postcodes.io/postcodes/' + postcode)
+        converted_postcode = request_postcode.json()
+        postcode = (converted_postcode['result']['postcode'])
+        return postcode
+    # except ConnectionError:
+    #     print('Not a valid URL')
 
 def write_to_file(file, postcode_item):
     try:
